4b4d319db8d1cf20d2497e9d672810dcb34c4454
vineet kushwaha <vineetkushwaha2010@gmail.com>
2025-07-12 22:50:43 +0530

    few changes

--- booking.js
+++ booking.js
@@ -1,20 +1,33 @@
 let FetchData=async()=>{
     let url='http://localhost:3000/Dr-Appointment'
+    let res=await fetch(url)
+    let data=await res.json()
+    datashow(data)
+}
+let searchin=async()=>{
+    let search=document.querySelector("#search").value.toLowerCase()
+    let url='http://localhost:3000/Dr-Appointment'
     let res=await fetch(url,{method:"GET"})
     let data=await res.json()
-    console.log(data)
+    let FilterData=data.filter((e)=>{
+        return e.Doctorname.toLowerCase().includes(search)
+    })
+    datashow(FilterData)
+}
+let datashow=(data)=>{
+let Show=document.querySelector("#dataShow")
+Show.innerHTML=""
 
-let Show=document.querySelector("#showdata")
 data.map((e)=>{
     Show.innerHTML+=`
     <tr>
         <td>${e.Name}</td>
-        <td>${e.Email}</td>
+        <td>${e.Age}</td>
         <td>${e.Phonenumber}</td>
         <td>${e.Date}</td>
         <td>${e.Time}</td>
         <td>${e.Doctorname}</td>
-        <td onclick="Update('${e.id}')">Update</td>
+        <td onclick="formopen('${e.id}')">Update</td>
         <td onclick="Del('${e.id}')">Delete</td>
         </tr>
     
@@ -22,25 +35,22 @@ data.map((e)=>{
     `
 })
 }
-let Update=(id)=>{
-let url=`http://localhost:3000/Dr-Appointment/${id}`
-fetch(url,{method:"UPDATE"})
-}
 
-let Del=(id)=>{
+let Del=async(id)=>{
 let url=`http://localhost:3000/Dr-Appointment/${id}`
-fetch(url,{method:"DELETE"})
+await fetch(url,{method:"DELETE"})
+FetchData()
 }
 
 let Ins=()=>{
     let name=document.querySelector("#name").value
-    let email=document.querySelector("#email").value
+    let age=document.querySelector("#age").value
     let Number=document.querySelector("#number").value
     let date=document.querySelector("#date").value
     let time=document.querySelector("#time").value
     let doctor=document.querySelector("#doctor").value
   
-if (!name || !email || !number || !date || time === "Select time slot" || doctor === "Select a Doctor") {
+if (!name || !age || !Number || !date || time === "Select time slot" || doctor === "Select a Doctor") {
     alert("Please fill in all required fields.");
     return false;
   }
@@ -54,7 +64,7 @@ fetch(url,{
     },
     body:JSON.stringify({
         Name:name,
-        Email:email,
+        Age:age,
         Phonenumber:Number,
         Date:date,
         Time:time,
@@ -68,8 +78,6 @@ return false
 }
 
 
-FetchData()
-
 // form open 
 
 let formopen=async(id)=>{
@@ -81,27 +89,27 @@ let formopen=async(id)=>{
      <div class="form-row">
      <div>
      Full Name <br>
-    <input type="text"  id="upname"  value="${data.name}" placeholder="Enter your full name"></div>
+    <input type="text"  id="upname"  value="${data.Name}" placeholder="Enter your full name"></div>
   <div>
-    Email Address <br>
-    <input type="text"  id="upemail" value="${data.email}" placeholder="Enter your email">
+    Age <br>
+    <input type="text"  id="upage" value="${data.Age}" placeholder="Enter your age">
   </div>
    </div>
     <br>
     <div class="form-row">
     Phone Number <br>
-    <input type="tel"  id="upNumber" value="${data.number}" placeholder="Enter your phone number">
+    <input type="tel"  id="upNumber" value="${data.Phonenumber}" placeholder="Enter your phone number">
     </div>
    <div>
     Preferred Date <br>
-    <input type="date" id="update" value="${data.date}" placeholder="dd-mm-yyyy">
+    <input type="date" id="update" value="${data.Date}" placeholder="dd-mm-yyyy">
    </div>
     </div>
     <br>
     <div class="form-row">
    <div class="form-group">
     Preferred Time <br>
-    <select  id="update">
+    <select  id="uptime">
         <option selected>Select time slot</option>
         <option>10:00 AM - 12:00 AM</option>
         <option>01:00 PM - 03:00 PM</option>
@@ -127,17 +135,18 @@ let formopen=async(id)=>{
     <textarea id="message" placeholder="briefly describe your symtoms or reason for the appiontment"></textarea>
    </div>
    <br>
-        <input type="Submit" value="update Appointment" class="submit-btn" onclick="return updatedata('${data.id}')">
+        <input type="Submit" value="update Appointment" class="submit-btn" onclick="return updateData('${data.id}')">
     
     
     
     `
+    document.querySelector("#updateForm").innerHTML=FormData
 }
 
-let updateData=(id)=>{
+let updateData=async(id)=>{
     let name=document.querySelector("#upname").value
-    let email=document.querySelector("#upemail").value
-    let Number=document.querySelector("#upnumber").value
+    let age=document.querySelector("#upage").value
+    let Number=document.querySelector("#upNumber").value
     let date=document.querySelector("#update").value
     let time=document.querySelector("#uptime").value
     let doctor=document.querySelector("#updoctor").value
@@ -145,14 +154,14 @@ let updateData=(id)=>{
     let url=`http://localhost:3000/Dr-Appointment/${id}`
 
 
-    fetch(url,{
+    await fetch(url,{
         method:"PUT",
         headers:{
         "Content-type":"application/json"
     },
     body:JSON.stringify({
         Name:name,
-        Email:email,
+        Age:age,
         Phonenumber:Number,
         Date:date,
         Time:time,
